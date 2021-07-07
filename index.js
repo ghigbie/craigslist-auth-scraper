eea7fef53454a2c9a2207ef1b169c11a80d4d9ff
ghigbie <gehigbie@gmail.com>
2021-07-06 20:27:30 -0400

    Edit package.json

---
+++ index.js
@@ -0,0 +1,26 @@
+const request = require('request-promise');
+const fs = require("fs");
+const {username, password} = require('./creds');
+
+
+const main = async () => {
+    try{
+        const html = await request.post("https://accounts.craigslist.org/login", {
+            form: {
+                inputEmailHandle: username,
+                inputPassword: password,
+            },
+            headers: {
+                Referer: 'https://accounts.craigslist.org/login'
+            },
+            simple: false
+        });
+        fs.writeFileSync("./login.html", html)
+    }catch (error){
+        console.log('*********************************');
+        console.log('Error: ', error);
+        console.log('*********************************');
+    }
+}
+
+main();
