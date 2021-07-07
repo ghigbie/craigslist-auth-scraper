1122ce01390b810285f5e2a1be2874b0a4ce65be
ghigbie <gehigbie@gmail.com>
2021-07-06 20:28:32 -0400

    Edit settings

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const main = async () => {
             headers: {
                 Referer: 'https://accounts.craigslist.org/login'
             },
-            simple: false
+            simple: false,
+            followAllRedirects: true,
         });
         fs.writeFileSync("./login.html", html)
     }catch (error){
