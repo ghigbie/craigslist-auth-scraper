408e4a5871f9f5ec8ab732f6717235cdc68c6ffe
ghigbie <gehigbie@gmail.com>
2021-07-09 14:48:53 -0400

    Get web scraper working

--- automated.js
+++ automated.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
 const {username, password} = require('./creds');
 
 const loginPage = "https://accounts.craigslist.org/login";
@@ -9,12 +10,15 @@ const main = async () => {
         const browser = await puppeteer.launch({headless: false});
         const page = await browser.newPage();
         await page.goto(loginPage);
-        await page.type('input#inputEmailHandle', username, {delay: 2});
-        await page.type('input#inputPassword', password, {delay: 2});
+        await page.type('input#inputEmailHandle', username);
+        await page.type('input#inputPassword', password);
         await page.click('button#login');
-        await page.waitForNavigation();
-        const drafts = await page.goto(draftsPage);
-        console.log(drafts);
+        await page.goto(draftsPage);
+       // await page.waitForNavigation();
+        const drafts = await page.content();
+        const $ = cheerio.load(drafts);
+        const message = $('section > fieldset > b').text();
+        console.log(message);
     }catch (error){
         console.log('***************************');
         console.log('Error: ', error);
